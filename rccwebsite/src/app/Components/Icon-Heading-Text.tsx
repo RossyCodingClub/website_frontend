467b8b9b6550b77534af2d0b923d75ba986571c2
Nick Wolfy <qwerty1register@gmail.com>
2023-06-07 17:40:03 +0800

    changed the iconheadertext component to instead accept an image and not a path

--- rccwebsite/src/app/Components/Icon-Heading-Text.tsx
+++ rccwebsite/src/app/Components/Icon-Heading-Text.tsx
@@ -2,18 +2,20 @@ import { ReactNode } from 'react';
 import { Text, Image, useTheme } from '@nextui-org/react';
 
 interface IconHeadingTextProps {
-  iconPath: string;
-  headerContent: ReactNode;
-  textContent: ReactNode;
-}
+    icon: ReactNode;
+    headerContent: ReactNode;
+    textContent: ReactNode;
+  }
 
-const IconHeadingText: React.FC<IconHeadingTextProps> = ({ iconPath, headerContent, textContent }) => {
+const IconHeadingText: React.FC<IconHeadingTextProps> = ({ icon, headerContent, textContent }) => {
   const { theme } = useTheme();
 
   return (
     <div style={{ display: 'flex', alignItems: 'flex-start' }}>
       <div style={{ paddingRight: '3px' }}>
-        <Image src={iconPath} width={24} height={24} />
+        <div style={{width: '24px', height: "24px"}}>
+        {icon}
+        </div>
       </div>
       <div>
         <Text h2 color={theme?.colors.secondary.toString()}>{headerContent}</Text>
--- rccwebsite/src/app/Screens/LandingScreen.tsx
+++ rccwebsite/src/app/Screens/LandingScreen.tsx
@@ -30,12 +30,12 @@ function LandingScreen() {
         //Final call to action
         //footer (should be its own component that doesnt seem to change)
         <div style={{ display: 'grid', gridTemplateColumns: '1fr 1fr 1fr', gap: '16px' }}>
-        <IconHeadingText headerContent="Random header1" textContent="random text" iconPath="/rccwebsite/public/Assets/6c265cb9-e4c5-4efc-8cf5-d0afae6a4dd8.jpg" />
-        <IconHeadingText headerContent="Random header2" textContent="random text" iconPath="/rccwebsite/public/Assets/6c265cb9-e4c5-4efc-8cf5-d0afae6a4dd8.jpg" />
-        <IconHeadingText headerContent="Random header3" textContent="random text" iconPath="/rccwebsite/public/Assets/6c265cb9-e4c5-4efc-8cf5-d0afae6a4dd8.jpg" />
-        <IconHeadingText headerContent="Random header4" textContent="random text" iconPath="/rccwebsite/public/Assets/6c265cb9-e4c5-4efc-8cf5-d0afae6a4dd8.jpg" />
-        <IconHeadingText headerContent="Random header5" textContent="random text" iconPath="/rccwebsite/public/Assets/6c265cb9-e4c5-4efc-8cf5-d0afae6a4dd8.jpg" />
-        <IconHeadingText headerContent="Random header6" textContent="random text" iconPath="/rccwebsite/public/Assets/6c265cb9-e4c5-4efc-8cf5-d0afae6a4dd8.jpg" />
+        <IconHeadingText headerContent="Random header1" textContent="random text" icon={<img src="../../../public/Assets/6c265cb9-e4c5-4efc-8cf5-d0afae6a4dd8.jpg"></img>} />
+        <IconHeadingText headerContent="Random header2" textContent="random text" icon={<img src="../../../public/Assets/6c265cb9-e4c5-4efc-8cf5-d0afae6a4dd8.jpg"></img>} />
+        <IconHeadingText headerContent="Random header3" textContent="random text" icon={<img src="../../../public/Assets/6c265cb9-e4c5-4efc-8cf5-d0afae6a4dd8.jpg"></img>} />
+        <IconHeadingText headerContent="Random header4" textContent="random text" icon={<img src="../../../public/Assets/6c265cb9-e4c5-4efc-8cf5-d0afae6a4dd8.jpg"></img>} />
+        <IconHeadingText headerContent="Random header5" textContent="random text" icon={<img src="../../../public/Assets/6c265cb9-e4c5-4efc-8cf5-d0afae6a4dd8.jpg"></img>} />
+        <IconHeadingText headerContent="Random header6" textContent="random text" icon={<img src="../../../public/Assets/6c265cb9-e4c5-4efc-8cf5-d0afae6a4dd8.jpg"></img>} />
       </div>
       }
     </>
